9f4e2e338685da10dc352718a8ff892aaecca36e
unknown <55wokqe9@anonaddy.me>
2024-08-05 13:36:21 +0200

    Use true bpm instead of multiplier
    The term multiplier can be confusing as to the role of what it actually does etc. True BPM should be much more easily understandable, especially when contrasted with the term karaoke BPM, hopefully providing a better UX. The only issue is the requirement for the karaoke BPM to be an integer multiple of the true BPM, but that is enforced through an error message.

--- karaluxer.py
+++ karaluxer.py
@@ -62,8 +62,8 @@ class KaraLuxer():
         force_dialogue_lines: bool = False,
         tv_sized: bool = False,
         autopitch: bool = False,
-        bpm: float = 1500,
-        bpm_multiplier: int = 1
+        karaoke_bpm: float = 1500,
+        song_bpm: float = 1500
     ) -> None:
         """Sets up the KaraLuxer instance.
 
@@ -88,11 +88,11 @@ class KaraLuxer():
             tv_sized (bool, optional): If True will append (TV) to the song title. (This is the convention that
                 ultrastar.es uses).
             autopitch (bool, optional): If True Karaluxer will attempt to use ultrastar_pitch to pitch the notes.
-            bpm (float, optional): The BPM (beats per minute) of the song that will be used instead of the default 1500
-                BPM. For ultrastar maps, this tends to be approximately 300, even if the true BPM is often much lower.
-            bpm_multiplier (int, optional): The BPM multiplier, or the multiplier of the karaoke BPM with respect to
-                the true BPM. In other words: $\frac{karaoke BPM}{true BPM}$. Having the karaoke BPM a 3 or 4 multiple
-                of the true BPM allows for easier creation of gaps in between notes. Providing this option will allow
+            karaoke_bpm (float, optional): The BPM (beats per minute) for the karaoke txt file that will be used instead
+                of the default 1500 BPM. For ultrastar maps, this tends to be approximately 300, even if the true BPM
+                is often much lower.
+            song_bpm (float, optional): The actual BPM of the song/audio. Having the karaoke BPM a 3 or 4 multiple
+                of the Song BPM allows for easier creation of gaps in between notes. Providing this option will allow
                 KaraLuxer to arrange the notes more closely to the correct timing.
         """
 
@@ -112,8 +112,15 @@ class KaraLuxer():
         self.force_dialogue_lines = force_dialogue_lines
         self.tv_sized = tv_sized
         self.autopitch = autopitch
-        self.bpm = bpm
-        self.bpm_multiplier = bpm_multiplier
+        self.bpm = karaoke_bpm
+
+        self.bpm_multiplier = karaoke_bpm / song_bpm
+        if self.bpm_multiplier % 1 < 1e-5:
+            self.bpm_multiplier = int(self.bpm_multiplier)
+        else:
+            raise ValueError('The Karaoke BPM must be an integer multiple of the Song BPM (provided multiple='
+                             f'{self.bpm_multiplier}). Either provide a valid combination of Karaoke and Song BPM or '
+                             f'do not set the Song BPM.')
 
         # Parameter checks
         if kara_url and not re.match(r'https:\/\/kara\.moe\/kara\/[\w-]+\/[\w-]+', kara_url):
@@ -686,12 +693,14 @@ def main() -> None:
     argument_parser.add_argument('-tv', '--tv_sized', action='store_true', help='Mark this song as TV sized.')
     argument_parser.add_argument('-ap', '--autopitch',
                                  action='store_true', help='Pitch the song using ultrastar_pitch.')
-    argument_parser.add_argument('--bpm', type=float, default=1500.,
-                                 help='The BPM of the song. If not provided, 1500 will be used as default.')
-    argument_parser.add_argument('--bpm-multiplier', type=int, default=1,
-                                 help='The integer by which the true song BPM was multiplied to obtain the provided '
-                                      '(the "--bpm" option) karaoke BPM. If provided, this multiplier is used to '
-                                      'remove overlaps and otherwise clean up the timings to make mapping easier.')
+    argument_parser.add_argument('--karaoke-bpm', type=float, default=1500.,
+                                 help='The Karaoke BPM, i.e. the BPM that will be used in the karaoke txt file. '
+                                      'If not provided, 1500 will be used as default.')
+    argument_parser.add_argument('--song-bpm', type=int, default=0,
+                                 help='The true Song BPM, i.e. the BPM of the song. This should be different from the '
+                                      'Karaoke BPM; Karaoke BPM has to be an integer multiple of the Song BPM. '
+                                      'If provided, this is used to calculate the multiple which is used to remove '
+                                      'overlaps and otherwise clean up the timings to make mapping easier.')
 
     argument_parser.set_defaults(ignore_overlaps=False, force_dialogue=False, tv_sized=False, autopitch=False)
 
@@ -708,8 +717,8 @@ def main() -> None:
         arguments.force_dialogue,
         arguments.tv_sized,
         arguments.autopitch,
-        arguments.bpm,
-        arguments.bpm_multiplier
+        arguments.karaoke_bpm,
+        arguments.song_bpm if arguments.song_bpm != 0 else arguments.karaoke_bpm
     )
 
     def cli_overlap_decision_function(overlapping_lines: List[ass.line._Event]) -> ass.line._Event:
